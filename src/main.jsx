d186e6312b59c1d2d6210c3853d59a33dde41a8d
evgenia-fienik <160489180+evgenia-fienik@users.noreply.github.com>
2024-10-03 10:57:35 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import{BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { store, persistor } from './redux/store.js'
 import App from './components/App/App.jsx'
 import './index.css'
+import { PersistGate } from 'redux-persist/integration/react';
 import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Provider store={store}> 
+    <Provider store={store}> 
+      <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
         <HelmetProvider>
            <App />
      </HelmetProvider>
-    </BrowserRouter>
+        </BrowserRouter>
+        </PersistGate>
     </Provider>
     </React.StrictMode>
 );
